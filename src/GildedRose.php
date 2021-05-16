fdfffa00ffe2900f006b6857cf8b0da73a7a04ef
Natan <natan.morote@gmail.com>
2021-05-16 20:21:10 +0200

    split function to make it readable

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -8,6 +8,7 @@ class GildedRose {
     private $ab = 'Aged Brie';
     private $bptatc = 'Backstage passes to a TAFKAL80ETC concert';
     private $shor = 'Sulfuras, Hand of Ragnaros';
+    private $max_quality = 50;
 
 
     function __construct($items) {
@@ -17,23 +18,9 @@ class GildedRose {
     function update_quality() {
         foreach ($this->items as $item) {
             if ($item->name != $this->ab and $item->name != $this->bptatc) {
-                if ($item->quality > 0) {
-                    if ($item->name != $this->shor) {
-                        $item->quality--;
-                    }
-                }
+                $this->remove_quality($item);
             } else {
-                if ($item->quality < 50) {
-                    $item->quality++;
-                    if ($item->name == $this->bptatc) {
-                        if ($item->sell_in < 11) {
-                                $item->quality++;
-                        }
-                        if ($item->sell_in < 6) {
-                                $item->quality++;
-                        }
-                    }
-                }
+                $this->add_quality($item);
             }
 
             if ($item->name != $this->shor) {
@@ -41,26 +28,48 @@ class GildedRose {
             }
 
             if ($item->sell_in < 0) {
-                if ($item->name != $this->ab) {
-                    if ($item->name != $this->bptatc) {
-                        if ($item->quality > 0) {
-                            if ($item->name != $this->shor) {
-                                $item->quality--;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
+                $this->not_sell_in($item);
+            }
+        }
+    }
+
+    private function remove_quality($item) {
+        if ($item->quality) {
+            if ($item->name != $this->shor) {
+                $item->quality--;
+            }
+        }
+    }
+
+    private function add_quality($item) {
+        if ($item->quality < $this->max_quality) {
+            $item->quality++;
+            if ($item->name == $this->bptatc) {
+                if ($item->sell_in < 11) {
                         $item->quality++;
-                    }
+                }
+                if ($item->sell_in < 6) {
+                        $item->quality++;
                 }
             }
         }
     }
 
-    private function quality_check($item) {
-        
-    } 
+    private function not_sell_in($item) {
+        if ($item->name != $this->ab) {
+            if ($item->name != $this->bptatc) {
+                if ($item->quality) {
+                    if ($item->name != $this->shor) {
+                        $item->quality--;
+                    }
+                }
+            } else {
+                $item->quality = $item->quality - $item->quality;
+            }
+        } else {
+            if ($item->quality < $this->max_quality) {
+                $item->quality++;
+            }
+        }
+    }
 }
